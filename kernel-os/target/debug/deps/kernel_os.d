643f4912b122525352a1e254d9d00fdcb35a2c99
johannax94 <jmeguedad1@myges.fr>
2025-12-29 19:20:30 +0100

    test

---
+++ kernel-os/target/debug/deps/kernel_os.d
@@ -0,0 +1,5 @@
+D:\COURS_4SIE\RUST\kernel_rust\kernel_os\OS_in_Rust\kernel-os\target\debug\deps\kernel_os.exe: src\main.rs
+
+D:\COURS_4SIE\RUST\kernel_rust\kernel_os\OS_in_Rust\kernel-os\target\debug\deps\kernel_os.d: src\main.rs
+
+src\main.rs:
